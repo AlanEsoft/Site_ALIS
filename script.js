5576ba6adc920c290975596617030287fd5730b5
AlanEsoft <107324230+AlanEsoft@users.noreply.github.com>
2024-08-28 11:24:55 -0300

    Ajustes de layout
    Ajuste na logo e na div servicos.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const imagens = ['imagens/data_analytics.png', 'imagens/computer_class.png', 'imagens/courses.png', 'imagens/computer_mainetence.png',
-                     'imagens/computer_network.png'];
+                'imagens/computer_network.png'];
     const titulos = ['ANÁLISE DE DADOS E ENGENHARIA DE SOLUÇÕES', 'TREINAMENTO EM INFORMÁTICA', 'CURSOS E TREINAMENTOS EM GERAL', 
-                    'MANUTENÇÃO DE COMPUTADORES', 'INSTALAÇÃO E MANUTENÇÃO DE REDES DE COMPUTADORES'];
+                    'MANUTENÇÃO DE COMPUTADORES', 'INSTALAÇÃO E MANUTENÇÃO DE REDES DE COMPUTADORES'];
     const descricoes = ['Análise dos processos e edição de documentos, cadastros, banco de dados, planilhas, softwares entre outras '+
-                       ' ferramentas para aumentar lucros e reduzir custos.', 'Instrução nas principais ferramentas para que seus '+
-                       'colaboradores desempenham suas funções com eficiência.', 'Ministração de aulas em diversas áreas do conhecimento'+
-                       ' visando capacitá-lo para qualquer desafio. Desde reforço escolar a Business Inteligence.', 'Reparo e manutenção'+
-                    ' de computadores e periféricos com possibilidade de disponibilidade 24/7.', 'Instalação e manutenção de redes de '+
-                    'computadores e IoT (Dispositos em geral)'];
+                        ' ferramentas para aumentar lucros e reduzir custos.', 'Instrução nas principais ferramentas para que seus '+
+                        'colaboradores desempenham suas funções com eficiência.', 'Ministração de aulas em diversas áreas do conhecimento'+
+                        ' visando capacitá-lo para qualquer desafio. Desde reforço escolar a Business Inteligence.', 'Reparo e manutenção'+
+                        ' de computadores e periféricos com possibilidade de disponibilidade 24/7.', 'Instalação e manutenção de redes de '+
+                        'computadores e IoT (Dispositos em geral)'];
     const imagensServicos = document.querySelectorAll('.imgServices');
     const botoes = document.querySelectorAll('.btn');
     const titulo = document.querySelector('.titulo');
@@ -41,6 +41,19 @@ const imagens = ['imagens/data_analytics.png', 'imagens/computer_class.png', 'im
         setTimeout(iniciar, 30000);
          });
     });
+
+    function isMobile() {
+        return /Mobi|Android/i.test(navigator.userAgent);
+    }
+
+    window.onload = function() {
+        if (isMobile()) {
+            var imagelogo = document.getElementsByClassName('imglogo');
+            for (var i = 0; i < imagelogo.length; i++) {
+                imagelogo[i].src = 'imagens/LogoALIS_Tec90.png';
+            }
+        }
+    };
     
     iniciar();
     
